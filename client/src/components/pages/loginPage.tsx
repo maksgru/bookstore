b29e8764ec851c691cada51b3816afb94938f0bd
Maks <maksgru@hotmal.com>
2020-11-03 00:08:34 +0300

    loginpage

--- client/src/components/pages/loginPage.tsx
+++ client/src/components/pages/loginPage.tsx
@@ -28,35 +28,36 @@ export default class LoginPage extends Component {
     return (
       <Container fluid>
         <Jumbotron className="d-flex align-items-center justify-content-center">
-          <Form onSubmit={this.login}>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                name="email"
-                onChange={this.handleChange}
-                type="email"
-                placeholder="Enter email"
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                name="password"
-                onChange={this.handleChange}
-                type="password"
-                placeholder="Password"
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit" block>
-              Submit
-            </Button>
-          </Form>
+              <Form onSubmit={this.login} style={{minWidth: "25%"}}>
+              <div style={{textAlign: "center", marginBottom: "2rem"}}>
+                <h1>book STORE</h1>
+              </div>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Control
+                    name="email"
+                    onChange={this.handleChange}
+                    type="email"
+                    placeholder="Enter email"
+                  />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Control
+                    name="password"
+                    onChange={this.handleChange}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Group>
+                <Form.Group controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit" block>
+                  Submit
+                </Button>
+              </Form>
         </Jumbotron>
       </Container>
     );
